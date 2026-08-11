41822524af124a5263fcd031413573cdf16d0784
Dee <xiongdominie@gmail.com>
2026-08-11 12:34:10 -0500

    Rename CI/CD IAM role and update GitHub Actions

--- terraform/github-actions.tf
+++ terraform/github-actions.tf
@@ -41,7 +41,7 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
 }
 
 resource "aws_iam_role" "github_actions" {
-  name = "clearpointtraining-github-actions-role"
+  name = "clearpointtraining-cicd-role"
 
   assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
 }
