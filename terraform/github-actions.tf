4cd5b96b412e27d79634aa1c16c5af7ed31b2871
Dee <xiongdominie@gmail.com>
2026-08-12 16:57:43 -0500

    Update website to verify CI/CD deployment

--- terraform/github-actions.tf
+++ terraform/github-actions.tf
@@ -21,7 +21,7 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
 
     condition {
       test     = "StringEquals"
-      variable = "token.actions.gethubusercontent.com:aud"
+      variable = "token.actions.githubusercontent.com:aud"
 
       values = [
         "sts.amazonaws.com"
@@ -96,4 +96,6 @@ resource "aws_iam_role_policy_attachment" "github_actions" {
   role       = aws_iam_role.github_actions.name
   policy_arn = aws_iam_policy.github_actions.arn
 }
+
+
 
